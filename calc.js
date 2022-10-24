e8b6b8f47384fe3f7bc2e12052c93df10a507abb
NIJIN JOY <nijinjoy1999@gmail.com>
2022-10-24 19:41:24 +0530

    initial commit

--- calc.js
+++ calc.js
@@ -10,10 +10,14 @@ function evaluateexp(){
     //answer.value= res
     answer.value=eval(answer.value)
 }
-// function removeElement(){
-//     curr_exp=res.value
-//     answer.value=curr_exp.slice(0,-1)
-// }
-function backSpace(){
-    screenView.value = screenView.value.slice(0, - 1);
+function removeElement(){
+    curr_exp=res.value
+    answer.value=curr_exp.toString().slice(0,-1)
 }
+// function backSpace(){
+//     screenView.value = screenView.value.slice(0, - 1);
+// }
+// display.innerText = display.innerText.slice(0, -1)
+// function delete(){
+//     this.currentOperand = this.currentOperand.toString().slice(0, -1)
+//     }
